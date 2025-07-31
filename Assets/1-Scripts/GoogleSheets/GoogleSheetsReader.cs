307dc05415ac2fdc72a03b8b14f5ae382652904d
Jaime Camacho <jaime.camacho.dev@gmail.com>
2025-07-31 14:52:11 +0200

    Add UnityWebRequest module dependency

---
+++ Assets/1-Scripts/GoogleSheets/GoogleSheetsReader.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking; // requiere el paquete integrado "Unity Web Request"
+
+public class GoogleSheetsReader : MonoBehaviour
+{
+    [Tooltip("Public Google Sheets export link")] 
+    public string sheetUrl = "https://docs.google.com/spreadsheets/d/13PsACix0amVNjdoSWasaFPId-VAtGShmd6gMxgkFNy8/export?format=csv";
+
+    void Start()
+    {
+        StartCoroutine(GetSheetData());
+    }
+
+    IEnumerator GetSheetData()
+    {
+        UnityWebRequest request = UnityWebRequest.Get(sheetUrl);
+        yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError($"Error reading sheet: {request.error}");
+        }
+        else
+        {
+            string data = request.downloadHandler.text;
+            Debug.Log($"Sheet data:\n{data}");
+            // TODO: parse CSV data as needed
+        }
+    }
+}
